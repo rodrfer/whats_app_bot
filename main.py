ac77e90b4d351f3bce370463282453acc29a9f60
Rodrigo Ferretiz <59719911+rodrfer@users.noreply.github.com>
2022-01-11 12:44:38 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import pywhatkit
+import random
+
+
+
+
+
+
+
+a = random.choice(["Buenos Dias Guapa, ten un increíble día",
+                   "Hola espero estes teniendo un dia muy padre",
+                   "Ojala te vaya super bien hoy",
+                   "Eres increíble, buenos dias",
+                   "Recuerda que te quiero mucho, que te vaya super hoy",
+                   "Te quiero, buenos dias",
+                   "La vida nos está bendiciendo con un nuevo día juntos. Es maravilloso que Dios nos quiera tanto como para darnos este regalo. Abre los ojos, que quiero hundirme en ellos desde ahora mismo.",
+                   "Buen día, hermosa, el sol ha iluminado la habitación y tú has vuelto a iluminar mi vida."])
+
+
+
+#print(a)
+
+
+
+
+
+
+
+
+
+pywhatkit.sendwhatmsg('+5284885859',a,10,39)
+
+
+
